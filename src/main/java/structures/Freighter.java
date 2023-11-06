6e7c43ffec8a4a5a023b9cfd8d032ba4deb28e13
brandon mazey <brandon@brandons-Air.localdomain>
2023-11-06 10:15:36 -0500

    added TODOs

--- src/main/java/structures/Freighter.java
+++ src/main/java/structures/Freighter.java
@@ -25,29 +25,32 @@ public class Freighter {
 
     // push() adds an item to the Containers list; pay close attention to the order!
     public void push(String item) {
-        
+        // TODO implement push()
     }
 
     // pop() removes an item from the Containers list; pay close attention to the order!
     // pop() should return an empty String instead of an error if you try to pop an
     // item off an empty Containers stack
     public String pop() {
+        // TODO implement pop()
         return "";
     }
 
     // top() reads the next item to be unloaded from Containers
     public String top() {
+        // TODO implement top()
         return "";
     }
 
     // bottom() reads the final item to be unloaded from Containers
     public String bottom() {
+        // TODO implement bottom()
         return "";
     }
 
-    // isEmpty() returns true if Containers is empty, and flase if otherwise
+    // isEmpty() returns true if Containers is empty, and false if otherwise
     public boolean isEmpty() {
+        // TODO implement isEmpty()
         return true;
     }
-
 }
