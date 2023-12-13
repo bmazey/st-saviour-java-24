8b4817af23837424239cd470350600a20bd6aa07
AMVenetian <antonellavenetian@gmail.com>
2023-12-13 10:43:02 -0500

    updates to entertainment

---
+++ src/main/java/oop/entertainment/entertainment.java
@@ -0,0 +1,20 @@
+package oop.entertainment;
+
+public abstract class entertainment{
+    
+    private String creative;
+    
+    public String getCreative(){
+        return this.creative;
+    }
+    public void setCreative(String creative){
+        this.creative = creative;
+    }
+    private String pasttime;
+    public String getPasttime(){
+        return this.pasttime;
+    }
+    public void setPasttime(String pasttime){
+        this.pasttime = pasttime;
+    }
+}
