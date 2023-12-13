8af994e49071fdbcb60ccde7940fa8bbf235f215
AFWood <audreyfwood@gmail.com>
2023-12-13 10:42:32 -0500

    updates to entertainment file

---
+++ src/main/java/oop/entertainment/entertainment.java
@@ -0,0 +1,21 @@
+package oop.entertainment;
+
+public class entertainment {
+    
+    private String creative;
+
+    public String getCreative(){
+        return this.creative;
+    }
+    public void setCreative(String creative){
+        this.creative = creative;
+    }
+    private String pasttime;
+    public String getPasttime(){
+
+        return this.pasttime;
+    }
+    public void setPasttime(String pasttime){
+        this.pasttime = pasttime;
+    }
+}
