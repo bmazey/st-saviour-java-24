8e48c48d84b901800c205ffa2e6030f801563455
slandai2 <samara.landais@gmail.com>
2023-12-13 11:00:02 -0500

    continued drinks

---
+++ src/main/java/oop/mammals/Beverages/Drinks.java
@@ -0,0 +1,23 @@
+package oop.mammals.Beverages;
+
+public class Drinks {
+    
+    private String Flavor;
+
+    public String getFlavor(){
+        return this.Flavor;
+    }
+    
+    public void setFlavor(String Flavor){
+        this.Flavor = Flavor;
+    }
+
+    public String {
+        
+    }
+
+    public String smell (String scent){
+        String text = this.Flavor + " smells like " + scent + "!";
+        return text;
+    }
+}
