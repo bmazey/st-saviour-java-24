1d5af9847b9e0ad8dbf00f0de5a5820c25b42e5c
aladinjng <jossnayah@gmail.com>
2023-12-18 09:42:36 -0500

    dec 18th

--- src/main/java/oop/automobiles/DumpTruck.java
+++ src/main/java/oop/automobiles/DumpTruck.java
@@ -2,4 +2,12 @@ package oop.automobiles;
 
 public final class DumpTruck extends Truck {
     
+    public DumpTruck(String drive) {
+        this.setDrive(drive);
+    }
+
+    public void dumpGarbage(String drive) {
+        DumpTruck waste = new DumpTruck(drive);
+        this.getLarge().add(waste);
+    }
 }
