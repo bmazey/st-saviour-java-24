774f25a7e1bd70ad853eff48d890ce50596dbf36
egalenyc <lexi.degale@gmail.com>
2023-12-18 09:31:59 -0500

    update

--- src/main/java/oop/automobiles/DumpTruck.java
+++ src/main/java/oop/automobiles/DumpTruck.java
@@ -1,8 +1,11 @@
 package oop.automobiles;
 
-public class DumpTruck extends Truck {
-    public Truck (String name) {
-        
+public final class DumpTruck extends Truck {
+    public DumpTruck(String drive) {
+        this.setDrive(drive);
     }
-    
+    public void dumpGarbage(String drive) {
+        DumpTruck waste = new DumpTruck(drive);
+        this.getLarge().add(waste);
+    }
 }
