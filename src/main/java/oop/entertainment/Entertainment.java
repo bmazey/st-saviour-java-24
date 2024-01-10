6309acda229f9d9a8e7a4c4ca14ade684ed52ecf
AFWood <audreyfwood@gmail.com>
2024-01-10 08:35:52 -0500

    added comments to entertainment

--- src/main/java/oop/entertainment/Entertainment.java
+++ src/main/java/oop/entertainment/Entertainment.java
@@ -1,20 +1,38 @@
 package oop.entertainment;
 
+/*
+ * creates a class called entertainment that cannot have an instance only described as that
+ */
 public class Entertainment {
-    
+    /* 
+     * sets up the class attribute "title," as all entertainment has a title
+     */ 
     private String title;
-
-    public String getTitle(){
+    /* 
+     * uses the getter method for title - accesses the private specific variable title
+     */
+    public String getTitle() {
         return this.title;
     }
-    public void setTitle(String title){
+    /*
+     * uses the setter method - sets a new instance of title
+     */
+    public void setTitle(String title) {
         this.title = title;
     }
+    /*
+     * sets up the class attribute "pasttime," as all entertainment is a pasttime
+     */
     private String pasttime;
-    public String getPasttime(){
-
+    /* 
+     * uses the getter method for pasttime - accesses the private specific variable title
+     */
+    public String getPasttime() {
         return this.pasttime;
     }
+    /*
+     * sets up the class attribute "pasttime," as all entertainment is a pasttime
+     */
     public void setPasttime(String pasttime){
         this.pasttime = pasttime;
     }
