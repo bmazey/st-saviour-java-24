78c642155f93cc7b02b96ea4c1970ad6a3e86903
aladinjng <jossnayah@gmail.com>
2024-01-12 10:49:20 -0500

    finished tests

--- src/main/java/oop/automobiles/ConcreteTruck.java
+++ src/main/java/oop/automobiles/ConcreteTruck.java
@@ -1,18 +1,18 @@
 package oop.automobiles;
 
 public final class ConcreteTruck extends Truck {
-
-    // Constructor method for 
+ 
+    // Using constructor method for ConcreteTruck
     public ConcreteTruck(String license, int wheels) {
         super(license, wheels);
     }
 
     public void mixConcrete(String item) {
-        this.getCargo().add(item);
+        this.getCargo().add(item);  // Adds an item to the cargo list (loads and mixes concrete)
     }
 
     public void carryConcrete(String item) {
         System.out.println("This truck can transport concrete to the construction site!");
-        this.getCargo().clear();
+        this.getCargo().clear();    // Clears all items from the cargo list (transports concrete to a site)
     }
 }
--- src/main/java/oop/automobiles/DumpTruck.java
+++ src/main/java/oop/automobiles/DumpTruck.java
@@ -2,15 +2,16 @@ package oop.automobiles;
 
 public final class DumpTruck extends Truck {
 
+    // Using constructor method for DumpTruck
     public DumpTruck(String license, int wheels) {
         super(license, wheels);
     }
 
     public void loadGarbage(String item) {
-        this.getCargo().add(item); // Adds an item to the cargo list (loads garbage)
+        this.getCargo().add(item);  // Adds an item to the cargo list (loads garbage)
     }
 
     public void dumpGarbage(String item) {
-        this.getCargo().clear(); // Removes all items from the cargo list (dumps all the garbage)
+        this.getCargo().clear();    // Removes all items from the cargo list (dumps all the garbage)
     }
 }
